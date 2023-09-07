5b6d880621cf5339fed61155df8b715901e214be
deverzius <nguyenmanh.4949@gmail.com>
2023-09-07 18:46:26 +0700

    update: readme

--- dbconnection.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-URI = "mongodb+srv://manhnguyenkimewaza:manh4949@cluster0.iwnzpbk.mongodb.net/?retryWrites=true&w=majority"
-
-client = MongoClient(URI)
-client.close()

09af7bc09bbf5ab229d537a117206aad3b62d4b3
deverzius <nguyenmanh.4949@gmail.com>
2023-09-11 15:08:49 +0700

    update: serializers + add: delete method

--- user/serializers.py
+++ user/serializers.py
@@ -2,17 +2,8 @@ from rest_framework.serializers import ModelSerializer
 
 from .models import User
 
-# chua co gi dac biet lam
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            "username",
-            "password",
-            "full_name",
-            "age",
-            "email",
-            "number",
-            "address",
-            "created_at"
-        )
+        # fields = '__all__'
+        exclude = ['id']

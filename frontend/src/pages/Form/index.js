5e1bf555d536c462c65fa39d61d4173343712052
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2023-09-12 15:59:16 +0700

    basic fe structure

---
+++ frontend/src/pages/Form/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Input } from "antd";
+
+const Form = () => {
+    return (
+        <div>
+            <h1>pages/Form</h1>
+            <Input placeholder="Project name" />
+            
+        </div>
+    )
+}
+
+export default Form;

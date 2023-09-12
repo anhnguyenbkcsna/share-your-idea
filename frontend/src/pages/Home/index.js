8bad7c7f51f94e7f49b3248e73079e8f2eba2766
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2023-09-12 15:59:16 +0700

    basic fe structure

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Home = () => {
+    return (
+        <div>
+            <p>pages/Home</p>
+        </div>
+    )
+}
+
+export default Home;

cdd908915d9ad5badece9bcfc0d01b37ed40b7c4
deverzius <nguyenmanh.4949@gmail.com>
2023-09-16 16:46:02 +0700

    add: connect to aws s3

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,15 @@
 from pymongo import MongoClient
 from bson import json_util
+from dotenv import load_dotenv
+import os
 import json
+
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
 def connect_db():
-    CONNECTION_STRING = "mongodb+srv://manhnguyenkimewaza:manh4949@cluster0.iwnzpbk.mongodb.net/?retryWrites=true&w=majority"
+    load_dotenv()
+    CONNECTION_STRING = os.getenv("CONNECTION_STRING")
     client = MongoClient(CONNECTION_STRING)
     return client.get_database("share-your-idea")

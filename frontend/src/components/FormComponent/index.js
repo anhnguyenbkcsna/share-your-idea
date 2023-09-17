8f1738b011b912748f92f23cbd9b4b471916b7c7
Qila8nguyen <otokhung1805@gmail.com>
2023-09-17 14:05:11 +0700

    add CORS server, POST fileList

--- frontend/src/components/FormComponent/index.js
+++ frontend/src/components/FormComponent/index.js
@@ -3,10 +3,10 @@ import { Typography } from "antd";
 import { Button, message, Steps } from "antd";
 import TestForm from "./testForm";
 import {
-	Form,
-	Input,
-	Slider,
-	Upload,
+    Form,
+    Input,
+    Slider,
+    Upload,
 } from "antd";
 
 const steps = [
@@ -14,22 +14,23 @@ const steps = [
         title: "First",
         content: "Overview",
     },
-    {
-        title: "Second",
-        content: "Customer Segments",
-    },
-    {
-        title: "Third",
-        content: "Value Position",
-    },
-    {
-        title: "Last",
-        content: "Furthur Information",
-    },
+    // {
+    //     title: "Second",
+    //     content: "Customer Segments",
+    // },
+    // {
+    //     title: "Third",
+    //     content: "Value Position",
+    // },
+    // {
+    //     title: "Last",
+    //     content: "Furthur Information",
+    // },
 ];
 
 const FormComponent = () => {
     const [current, setCurrent] = useState(0);
+    const [fileList, setFileList] = useState([])
     const next = () => {
         setCurrent(current + 1);
     };
@@ -40,6 +41,26 @@ const FormComponent = () => {
         key: item.title,
         title: item.title,
     }));
+
+    const finishForm = async () => {
+        const data = new FormData();
+        data.append('image', new Blob(fileList), 'file_list_name')
+        console.log('------- data', fileList, data)
+        const result = await fetch('http://127.0.0.1:8000/idea/65057546c5ba16ab88f1cecc/', {
+            method: 'POST',
+            // headers: {
+            //     // "Content-Type": "application/json",
+            //     'Content-Type': 'multipart/form-data',
+            // },
+            body: data
+        }).then(res => {
+            console.log('>>>>>>> response', res)
+            return res.data
+        })
+
+        const obj = result ? JSON.parse(result) : 'empty'
+        console.log('>>>>> obj', obj)
+    }
 
     return (
         <div class="formContainer">
@@ -55,28 +76,28 @@ const FormComponent = () => {
                 }}
             >
                 <Typography.Title level={3}>
-                    
+
                 </Typography.Title>
                 <Steps current={current} items={items} />
-                
+
                 <Typography.Title level={3}>
                     {steps[current].content}
                 </Typography.Title>
-                
+
                 {/* Content */}
-                {current === 0 && 
-                    <TestForm />
+                {current === 0 &&
+                    <TestForm setFileList={setFileList} />
                 }
-                {current === 1 && 
-                    <TestForm/>
+                {current === 1 &&
+                    <TestForm setFileList={setFileList} />
                 }
-                {current === 2 && 
-                    <TestForm/>
+                {current === 2 &&
+                    <TestForm setFileList={setFileList} />
                 }
-                {current === 3 && 
-                    <TestForm/>
+                {current === 3 &&
+                    <TestForm setFileList={setFileList} />
                 }
-                
+
                 <div style={{ marginTop: 24 }}>
                     {current > 0 && (
                         <Button style={{ margin: "0 8px" }} onClick={() => prev()} >
@@ -85,11 +106,11 @@ const FormComponent = () => {
                     )}
                     {current < steps.length - 1 && (
                         <Button type="primary" onClick={() => next()}>
-                        Next
+                            Next
                         </Button>
                     )}
                     {current === steps.length - 1 && (
-                        <Button type="primary" onClick={() => message.success("Processing complete!")} >
+                        <Button type="primary" onClick={() => finishForm()} >
                             Done
                         </Button>
                     )}
--- frontend/src/components/FormComponent/testForm.js
+++ frontend/src/components/FormComponent/testForm.js
@@ -10,55 +10,62 @@ import {
 
 const normFile = (e) => {
 	if (Array.isArray(e)) {
+		console.log('>>>>>>  e?.fileList', e?.fileList)
 		return e;
 	}
 	return e?.fileList;
 };
 
-const TestForm = () => {
-return (
-	<Form
-		labelCol={{ span: 4 }}
-		wrapperCol={{ span: 14 }}
-		layout="horizontal"
-		style={{
-			minWidth: 600,
-			padding: "20px",
-		}}
-	>
-		<Form.Item label="Project name">
-			<Input />
-		</Form.Item>
-
-		<Form.Item label="Field">
-			<Input />
-		</Form.Item>
-
-		<Form.Item label="Project slogan">
-			<Input />
-		</Form.Item>
-
-		<Form.Item label="Description">
-			<Input aria-rowcount={4} />
-		</Form.Item>
-
-		<Form.Item label="Slider">
-			<Slider range step={1} defaultValue={[20, 50]} />
+const TestForm = (props) => {
+	const { setFileList } = props
+	const uploadFiles = async (upload) => {
+		const { file, fileList } = upload
+		setFileList(fileList)
+		console.log('>>>>>> file', fileList)
+	}
+	return (
+		<Form
+			labelCol={{ span: 4 }}
+			wrapperCol={{ span: 14 }}
+			layout="horizontal"
+			style={{
+				minWidth: 600,
+				padding: "20px",
+			}}
+		>
+			<Form.Item label="Project name">
+				<Input />
 			</Form.Item>
 
-		<Form.Item
-			label="Upload"
-			valuePropName="fileList"
-			getValueFromEvent={normFile}
-		>
-			<Upload action="/upload.do" listType="picture-card">
-				<div>
-					<PlusOutlined />
-					<div style={{ marginTop: 8 }}>Upload</div>
-				</div>
-			</Upload>
-		</Form.Item>
-	</Form>
+			<Form.Item label="Field">
+				<Input />
+			</Form.Item>
+
+			<Form.Item label="Project slogan">
+				<Input />
+			</Form.Item>
+
+			<Form.Item label="Description">
+				<Input aria-rowcount={4} />
+			</Form.Item>
+
+			<Form.Item label="Slider">
+				<Slider range step={1} defaultValue={[20, 50]} />
+			</Form.Item>
+
+			<Form.Item
+				label="Upload"
+				valuePropName="fileList"
+				getValueFromEvent={normFile}
+			>
+				<Upload listType="picture-card" onChange={(e) => uploadFiles(e)}>
+					<div>
+						<PlusOutlined />
+						<div style={{ marginTop: 8 }}>Upload</div>
+					</div>
+				</Upload>
+			</Form.Item>
+		</Form>
 	);
 };
 

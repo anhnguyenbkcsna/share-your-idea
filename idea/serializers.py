295da654f912a0b3147b86dde302cc8fe3f6ac23
deverzius <nguyenmanh.4949@gmail.com>
2023-09-17 16:41:06 +0700

    update: upload multifiles

--- idea/serializers.py
+++ idea/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Idea
 
 class IdeaSerializer(ModelSerializer):
+    files = serializers.ListField(
+        child=serializers.FileField(), required=False
+    )
+    
     class Meta:
         model = Idea
         exclude = ['id']

041c18a8d1e3889335e585f425735b5c8bbfabd2
Qila8nguyen <otokhung1805@gmail.com>
2023-09-17 08:40:22 +0700

    remove node-modules, build folder

---
+++ frontend/src/pages/UserForm/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DatePicker, Typography } from 'antd';
+
+const UserForm = () => {
+  return (
+    <div>
+      <DatePicker />
+      <Typography.Text type='' strong>Hello</Typography.Text>
+    </div>
+  )
+}
+
+export default UserForm

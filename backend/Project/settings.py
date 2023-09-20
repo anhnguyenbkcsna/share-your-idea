be79e291c796a7b0085b24faa4d653b8c1352cc4
Qila8nguyen <otokhung1805@gmail.com>
2023-09-20 22:55:30 +0700

    Dockerize App

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = "django-insecure-j$2p0jow_&anl9y=r*z%^k*@gud=ean7(*2j#rki=tazg$n)+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -138,11 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend", "build", "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend", "build", "static")]
 
-STATIC_URL = "static/"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Default primary key field type
+# Default primary key field types
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

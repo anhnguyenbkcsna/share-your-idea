b6b1dd517f1f3e7b848d3c9bc11d3ed3a9740e44
deverzius <nguyenmanh.4949@gmail.com>
2023-11-11 00:14:13 +0700

    fixbug: idea model field errors

--- backend/idea/serializers.py
+++ backend/idea/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-
 from .models import Idea
 
 
 class IdeaSerializer(ModelSerializer):
+    domain = serializers.JSONField()
+    professional = serializers.JSONField(required=False)
+    geographical = serializers.JSONField(required=False)
+    ageRange = serializers.JSONField(required=False)
+    outstand = serializers.JSONField(required=False)
     files = serializers.ListField(child=serializers.FileField(), required=False)
-
+    
     class Meta:
         model = Idea
         exclude = ["upvote", "downvote"]
+
+class StringListField(serializers.ListField):
+    child = serializers.CharField()
+    required = False

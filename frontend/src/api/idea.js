3a2de7c8b5e9c4824214a6c8a80608d6b25e2e8b
deverzius <nguyenmanh.4949@gmail.com>
2023-11-11 23:18:05 +0700

    fixbug: error when post files

--- frontend/src/api/idea.js
+++ frontend/src/api/idea.js
@@ -1,10 +1,32 @@
 import axios from 'axios'
 
-const backend = '172.16.16.57:8000'
+const backend = 'http://127.0.0.1:8000'
 
 export const createNewIdea = async (ideaObj) => {
+  const flattenIdeaObj = {}
+  Object.assign(flattenIdeaObj, ...Object.values(ideaObj))
+
+  const formData = new FormData()
+
+  // stringify all fields except files
+  for (let key in flattenIdeaObj)
+  {
+    if (key === 'files') continue
+    formData.append(key, JSON.stringify(flattenIdeaObj[key]))
+  }
+
+  // append files
+  for (let file of flattenIdeaObj.files)
+  {
+    formData.append('files', file.originFileObj, file.name)
+  }
+
   return await axios
-    .post(`${backend}/idea`, ideaObj)
+    .post(`${backend}/idea`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
     .then((res) => {
       console.log('res', res)
       return res

ba6deb03c0ffbe27af391455198e436c654d8d1d
deverzius <nguyenmanh.4949@gmail.com>
2024-03-04 17:45:21 +0700

    integrate get contests api + change event card style

--- frontend/src/components/Contest/EventCard/index.jsx
+++ frontend/src/components/Contest/EventCard/index.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import { Button } from 'antd'
+import { formatDate } from '../../../utils/utils'
+import { OrangeWhiteShadowButton } from '../../../pages/Contest/Components/button'
 
 const ContestEventCard = ({ contest }) => {
+  const {
+    status,
+    deadline,
+    location,
+    organizer,
+    note
+  } = contest
+  const dateInStr = formatDate(deadline)
+
   return (
     <div className={styles.cardContainer}>
       <div className={styles.headerContainer}>
-        <div className={styles.headerTitle}>Event Title</div>
-        <div className={styles.headerDate}>18/01/2024</div>
+        <div className={styles.headerTitle}>{status ? 'opening' : 'closed'}</div>
+        <div className={styles.headerDate}>{dateInStr}</div>
       </div>
-      <h3 className={styles.title}>GDSC HCM</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-        condimentum, nisl non tincidunt luctus, nunc dui lacinia massa, quis
-        aliquam arcu metus vel orci. Sed in metus in sapien tincidunt
-        condimentum. Sed in metus in sapien tincidunt condimentum.
-      </p>
+      <h3 className={styles.title}>{organizer}</h3>
+      <div className={styles.line}>
+        {`Hạn chót nộp ý tưởng: ${dateInStr}`}
+      </div>
+      <div className={styles.line}>
+        {`Địa điểm tổ chức: ${location}`}
+      </div>
+      <div className={styles.line}>
+        {`Chủ đề: ${location}`}
+      </div>
+      <div className={styles.line}>
+        {`Yêu cầu: ${note}`}
+      </div>
       <div className={styles.footerContainer}>
-        <Button type="primary" className={styles.footerButton}>View</Button>
+        <OrangeWhiteShadowButton
+          className={styles.footerButton}
+          style={{
+            margin: '0 auto'
+          }}
+          text='Xem' />
       </div>
     </div>
   )

7aad44aec1cee7997108f48600dc325c609df686
deverzius <nguyenmanh.4949@gmail.com>
2024-05-27 15:12:42 +0700

    feat: create gg meet link

--- frontend/src/pages/Meet/index.jsx
+++ frontend/src/pages/Meet/index.jsx
@@ -15,7 +15,7 @@ import {
     Space,
 } from "antd";
 import "./styles.css";
-import viVN  from "antd/lib/locale/vi_VN";
+import viVN from "antd/lib/locale/vi_VN";
 
 export default function MeetPage() {
     const [signedIn, setSignedIn] = useState(false);
@@ -45,6 +45,20 @@ export default function MeetPage() {
         });
     };
 
+    const handleFinish = async (event) => {
+        for (let key in event) {
+            event[key] = event[key] ?? "";
+        }
+        console.log(event);
+
+        try {
+            const response = await createGoogleMeetEvent(event);
+            console.log("Event created:", response);
+        } catch (error) {
+            console.error("Error creating event:", error);
+        }
+    };
+
     useEffect(() => {
         initGoogleClient();
     }, []);
@@ -53,62 +67,73 @@ export default function MeetPage() {
         <div>
             <h1>Meet Page</h1>
             <div>
-                {signedIn ? (
-                    <button onClick={handleCreateEvent}>
-                        Create Google Meet Event
-                    </button>
-                ) : (
-                    <button onClick={handleSignIn}>Sign In</button>
-                )}
+                {!signedIn && <button onClick={handleSignIn}>Sign In</button>}
             </div>
             <div>
-                <Form>
-                    <Form.Item
-                        name="summary"
-                        label="Tên sự kiện"
-                        required
-                        rules={[
-                            {
-                                type: "string",
-                                min: 10,
-                            },
-                        ]}
-                    >
-                        <Input placeholder="vd: gọi vốn cho ý tưởng" />
-                    </Form.Item>
-                    <Form.Item
-                        name="description"
-                        label="Mô tả"
-                        rules={[
-                            {
-                                type: "string",
-                                min: 10,
-                            },
-                        ]}
-                    >
-                        <Input placeholder="vd: gọi vốn cho ý tưởng" />
-                    </Form.Item>
-                    {/* <Form.Item name="start" label="Ngày giờ bắt đầu" required>
-                        <DatePicker name="start" showTime showNow />
-                    </Form.Item>
-                    <Form.Item name="end" label="Ngày giờ kết thúc" required>
-                        <DatePicker name="end" showTime />
-                    </Form.Item> */}
-                    <ConfigProvider locale={viVN}>
+                <ConfigProvider locale={viVN}>
+                    <Form onFinish={handleFinish}>
+                        <Form.Item
+                            name="summary"
+                            label="Tên sự kiện"
+                            required
+                            rules={[
+                                {
+                                    type: "string",
+                                    min: 10,
+                                },
+                            ]}
+                        >
+                            <Input placeholder="vd: gọi vốn cho ý tưởng" />
+                        </Form.Item>
+                        <Form.Item
+                            name="description"
+                            label="Mô tả"
+                            rules={[
+                                {
+                                    type: "string",
+                                    min: 10,
+                                },
+                            ]}
+                        >
+                            <Input placeholder="vd: mô tả cho sự kiện gọi vốn" />
+                        </Form.Item>
                         <Form.Item
                             name="start"
                             label="Ngày giờ bắt đầu"
                             required
                         >
-                            <DatePicker.RangePicker showTime />
+                            <DatePicker
+                                disabledDate={(current) => current < new Date()}
+                                name="start"
+                                showNow
+                                format="YYYY-MM-DD HH:mm"
+                                showTime={{
+                                    format: "HH:mm",
+                                }}
+                            />
                         </Form.Item>
-                    </ConfigProvider>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
+                        <Form.Item
+                            name="end"
+                            label="Ngày giờ kết thúc"
+                            required
+                        >
+                            <DatePicker
+                                disabledDate={(current) => current < new Date()}
+                                name="end"
+                                format="YYYY-MM-DD HH:mm"
+                                showTime={{
+                                    format: "HH:mm",
+                                }}
+                                showNow
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </ConfigProvider>
             </div>
         </div>
     );
